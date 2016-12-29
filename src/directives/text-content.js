81ebd6fbd88e2673455a90beafb0162572cc8baa
morsecodist <todd@morsecodist.io>
2016-12-29 14:34:32 -0500

    Switched to responsive design with css media selectors

--- src/directives/text-content.js
+++ src/directives/text-content.js
@@ -7,20 +7,6 @@ app.directive('textContent', ['$window', 'MobileCheck', ($window, MobileCheck) =
       two: '=',
       content: '='
     },
-    templateUrl: 'views/partials/text-content.html',
-    link: (scope, element, attrs) => {
-      function lineCheck() {
-        var boxWidth = 0;
-        document.querySelectorAll('div.flex > div.text-content').forEach((elem) => {
-          if(elem.offsetWidth > boxWidth) boxWidth = elem.offsetWidth;
-        });
-        if(boxWidth > $window.innerWidth - 220)
-          angular.element(document.querySelectorAll('.line')).addClass('hidden');
-        else
-          angular.element(document.querySelectorAll('.line')).removeClass('hidden');
-      }
-      angular.element($window).bind('resize', lineCheck);
-      if(!MobileCheck.check()) lineCheck();
-    }
+    templateUrl: 'views/partials/text-content.html'
   };
 }]);
