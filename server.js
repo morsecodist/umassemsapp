2c5a5e70822ee5383dc4047bddef5e99def3fb84
morsecodist <todd@morsecodist.io>
2016-12-29 13:28:40 -0500

    Removed separate bower components path

--- server.js
+++ server.js
@@ -6,5 +6,4 @@ var app = express();
 var port = 8080;
 
 app.use(express.static('public'));
-app.use(express.static('bower_components'));
 app.listen(port, function(){console.log('Listening on port: ', port);});
